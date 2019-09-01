4b7297a4e237b8eca348fc444c1a131745fb93ca
Sebastian Czech <sebaczech@gmail.com>
2019-09-01 22:28:57 +0200

    Dynamiczna lista struktur z ksiazkami

--- lubimyczytac/lubimyczytac_api.go
+++ lubimyczytac/lubimyczytac_api.go
@@ -24,11 +24,13 @@ func newLubimyCzytacBook() *lubimyCzytacBook {
 }
 
 func (book lubimyCzytacBook) String() string {
-	return fmt.Sprintf("Author: %s, title: %s, image: %s, book: %s", book.author, book.title, book.image, book.book)
+	return fmt.Sprintf("author: %s, title: %s, image: %s, book: %s", book.author, book.title, book.image, book.book)
 }
 
 // SzukajLubimyCzytac API dla lubimyczytac.pl do wyszukiwania ksiazek
-func SzukajLubimyCzytac(url string) {
+func SzukajLubimyCzytac(url string) []*lubimyCzytacBook {
+	books := []*lubimyCzytacBook{}
+
 	c := colly.NewCollector(
 		colly.AllowedDomains("lubimyczytac.pl"),
 	)
@@ -36,27 +38,33 @@ func SzukajLubimyCzytac(url string) {
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
 		if strings.Contains(e.Request.URL.String(), "/szukaj/") && len(e.Text) > 0 && strings.Contains(link, "ksiazka") && !strings.Contains(e.Text, "Dodaj książkę") && !strings.Contains(e.Text, "Kup książkę") {
-			fmt.Printf("Book link found: %q -> %s\n", e.Text, link)
+			// fmt.Printf("Book link found: %q -> %s\n", e.Text, link)
 			c.Visit(e.Request.AbsoluteURL(link))
 
 			book := newLubimyCzytacBook()
 			book.title = e.Text
 			book.book = link
-			fmt.Printf("Struct: %s\n", book)
+			// fmt.Printf("Struct: %s\n", book)
+			books = append(books, book)
 		}
 		if strings.Contains(e.Request.URL.String(), "/ksiazka/") && len(e.Text) > 0 && strings.Contains(link, "autor") && strings.Contains(e.Attr("itemprop"), "name") {
-			fmt.Printf("Author link found: %q -> %s\n", e.Text, link)
+			// fmt.Printf("Author link found: %q -> %s\n", e.Text, link)
 
 			book := newLubimyCzytacBook()
 			book.author = e.Text
-			fmt.Printf("Struct: %s\n", book)
-
+			// fmt.Printf("Struct: %s\n", book)
+			books = append(books, book)
 		}
 	})
 
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
+		// fmt.Println("Visiting", r.URL.String())
 	})
 
 	c.Visit(url)
+
+	c.Wait()
+	fmt.Printf("List of structs: %s\n", books)
+
+	return books
 }
