617f5e491c07fd13c9ae575cf69550438cb4f7b2
Sebastian Czech <sebaczech@gmail.com>
2019-09-01 22:41:30 +0200

    Dostosowanie testow do zmian w funkcji wyszukiwania

--- lubimyczytac/lubimyczytac_api_test.go
+++ lubimyczytac/lubimyczytac_api_test.go
@@ -5,9 +5,15 @@ import (
 )
 
 func TestSzukajLubimyCzytacDlaKonkretnegoTytułu(t *testing.T) {
-	SzukajLubimyCzytac("http://lubimyczytac.pl/szukaj/ksiazki?phrase=cz%C5%82owiek+nietoperz&main_search=1")
+	books := SzukajLubimyCzytac("http://lubimyczytac.pl/szukaj/ksiazki?phrase=cz%C5%82owiek+nietoperz&main_search=1")
+	if len(books) != 1 {
+		t.Errorf("found %d books; wanted 1", len(books))
+	}
 }
 
 func TestSzukajLubimyCzytacDlaFragmentuTytułu(t *testing.T) {
-	SzukajLubimyCzytac("http://lubimyczytac.pl/szukaj/ksiazki?phrase=cz%C5%82owiek+niet&main_search=1")
+	books := SzukajLubimyCzytac("http://lubimyczytac.pl/szukaj/ksiazki?phrase=cz%C5%82owiek+niet&main_search=1")
+	if len(books) <= 1 {
+		t.Errorf("found %d books; wanted more than 1", len(books))
+	}
 }
