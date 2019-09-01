261646d5c881684acd46126190f3f3bdb905c8db
Sebastian Czech <sebaczech@gmail.com>
2019-09-01 21:24:38 +0200

    Dodanie nowej struktury lubimyCzytacBook

--- lubimyczytac/lubimyczytac_api.go
+++ lubimyczytac/lubimyczytac_api.go
@@ -7,6 +7,26 @@ import (
 	"github.com/gocolly/colly"
 )
 
+type lubimyCzytacBook struct {
+	author string
+	title  string
+	image  string
+	book   string
+}
+
+func newLubimyCzytacBook() *lubimyCzytacBook {
+	return &lubimyCzytacBook{
+		author: "",
+		title:  "",
+		image:  "",
+		book:   "",
+	}
+}
+
+func (book lubimyCzytacBook) String() string {
+	return fmt.Sprintf("Author: %s, title: %s, image: %s, book: %s", book.author, book.title, book.image, book.book)
+}
+
 // SzukajLubimyCzytac API dla lubimyczytac.pl do wyszukiwania ksiazek
 func SzukajLubimyCzytac(url string) {
 	c := colly.NewCollector(
@@ -18,9 +38,19 @@ func SzukajLubimyCzytac(url string) {
 		if strings.Contains(e.Request.URL.String(), "/szukaj/") && len(e.Text) > 0 && strings.Contains(link, "ksiazka") && !strings.Contains(e.Text, "Dodaj książkę") && !strings.Contains(e.Text, "Kup książkę") {
 			fmt.Printf("Book link found: %q -> %s\n", e.Text, link)
 			c.Visit(e.Request.AbsoluteURL(link))
+
+			book := newLubimyCzytacBook()
+			book.title = e.Text
+			book.book = link
+			fmt.Printf("Struct: %s\n", book)
 		}
 		if strings.Contains(e.Request.URL.String(), "/ksiazka/") && len(e.Text) > 0 && strings.Contains(link, "autor") && strings.Contains(e.Attr("itemprop"), "name") {
 			fmt.Printf("Author link found: %q -> %s\n", e.Text, link)
+
+			book := newLubimyCzytacBook()
+			book.author = e.Text
+			fmt.Printf("Struct: %s\n", book)
+
 		}
 	})
 
