b5b4deea1420ce3081b20a20c163da70ac261354
Sebastian Czech <sebaczech@gmail.com>
2019-09-08 13:40:37 +0200

    Oddzielny plik do uruchamiani z konsoli

---
+++ console.go
@@ -0,0 +1,7 @@
+package main
+
+import "./lubimyczytac"
+
+func main() {
+	lubimyczytac.SzukajLubimyCzytac("http://lubimyczytac.pl/szukaj/ksiazki?phrase=cz%C5%82owiek+nietoperz&main_search=1")
+}
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "./lubimyczytac"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func searchBook(w http.ResponseWriter, r *http.Request) {
+	searchBy := mux.Vars(r)["searchBy"]
+
+	for _, singleEvent := range events {
+		if singleEvent.ID == eventID {
+			json.NewEncoder(w).Encode(singleEvent)
+		}
+	}
+}
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "API-golang")
+}
 
 func main() {
-	lubimyczytac.SzukajLubimyCzytac("http://lubimyczytac.pl/szukaj/ksiazki?phrase=cz%C5%82owiek+nietoperz&main_search=1")
+	//lubimyczytac.SzukajLubimyCzytac("http://lubimyczytac.pl/szukaj/ksiazki?phrase=cz%C5%82owiek+nietoperz&main_search=1")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
