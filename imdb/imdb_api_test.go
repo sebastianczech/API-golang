28b004f79b942601cdf4fa26f230716831ed9471
Sebastian Czech <sebaczech@gmail.com>
2019-10-16 23:46:05 +0200

    Przygtowanie 2 testow do sprawdzenie znalezienia dokladniego 0 i 1 tytulu filmu

---
+++ imdb/imdb_api_test.go
@@ -0,0 +1,19 @@
+package imdb
+
+import (
+	"testing"
+)
+
+func TestFindImdbFilmDlaNieznanegoTytulu(t *testing.T) {
+	films := FindImdbFilm("http://www.imdb.com/find?q=nieznany%20tytul")
+	if len(films) != 0 {
+		t.Errorf("found %d films; wanted 0", len(films))
+	}
+}
+
+func TestFindImdbFilmDlaKonkretnegoTytulu(t *testing.T) {
+	films := FindImdbFilm("http://www.imdb.com/find?q=zasada%20przyjemnosc")
+	if len(films) != 1 {
+		t.Errorf("found %d films; wanted 1", len(films))
+	}
+}
