c5e2476641d956f02dc2e02880cf155a32ab9049
Sebastian Czech <sebaczech@gmail.com>
2019-11-30 21:41:27 +0100

    Wyszukiwanie filmow w IMDB

--- imdb/imdb_api.go
+++ imdb/imdb_api.go
@@ -2,6 +2,8 @@ package imdb
 
 import (
 	"fmt"
+	"strings"
+	"unicode"
 
 	"github.com/gocolly/colly"
 )
@@ -35,7 +37,48 @@ func FindImdbFilm(url string) []*IdmbFilm {
 		colly.AllowedDomains("www.imdb.com"),
 	)
 
-	// ...
+	c.OnHTML("td[class]", func(e *colly.HTMLElement) {
+		itemprop := e.Attr("class")
+		if itemprop == "result_text" {
+			link := e.ChildAttr("a[href]", "href")
+			fmt.Printf("Film link found: %s -> %s\n", e.ChildText("a[href]"), link)
+
+			film := newIdmbFilm()
+			film.Film = e.Request.AbsoluteURL(link)
+			films = append(films, film)
+
+			c.Visit(e.Request.AbsoluteURL(link))
+		}
+	})
+
+	c.OnHTML("h1[class]", func(e *colly.HTMLElement) {
+		itemprop := e.Attr("class")
+		if itemprop == "long" {
+			fmt.Printf("Title found: %s\n", e.Text)
+
+			for _, film := range films {
+				if film.Film == e.Request.URL.String() {
+					film.Title = strings.TrimFunc(e.Text, func(r rune) bool {
+						return !unicode.IsLetter(r) && !unicode.IsNumber(r)
+					})
+				}
+			}
+		}
+	})
+
+	c.OnHTML("div[class]", func(e *colly.HTMLElement) {
+		itemprop := e.Attr("class")
+		if itemprop == "poster" {
+			link := e.ChildAttr("img[src]", "src")
+			fmt.Printf("Image link found: %s -> %s\n", e.ChildText("img[src]"), link)
+
+			for _, film := range films {
+				if film.Film == e.Request.URL.String() {
+					film.Image = e.Request.AbsoluteURL(link)
+				}
+			}
+		}
+	})
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
